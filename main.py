8e676c6d57d3307c68a6ff05ebeb0ed912ba947c
andrew <asc269@cornell.edu>
2023-03-14 23:24:28 -0400

    Refactor driver into class Bot - end_session() -> close driver - TODO -> finish Bot.login()

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from selenium import webdriver
-driver =  webdriver.Firefox()
-driver.get('https://pollev.com/home')
+from selenium.webdriver.common.by import By
+
+class Bot():
+  def __init__(self, poll_name=None):
+    self.driver =  webdriver.Firefox()
+    self.poll_name = poll_name
+  
+  def login(self):
+    print("logging in...")
+    self.driver.get(f'https://pollev.com/home')
+    login = self.driver.find_element(By.XPATH, "/html/body/pe-application/div/nav-wrapper/pe-navigation/nav/div[1]/pe-navigation-item[4]/div")
+    login.click()
+    print("click login")
+
+  def end_session(self):
+    print("closing...")
+    self.driver.close()
+    print("successfully closed")
+    
+
+
+if __name__ == "__main__":
+  bot= Bot(poll_name="4820sp")
+  bot.login()
+  input()
+  bot.end_session()
+  
+
