c51db7b6cb00e7724cfc357f8a1c712846116793
andrew <asc269@cornell.edu>
2023-03-15 12:18:47 -0400

    Create Credentials class - add function signatures to check and create credentials

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-POLL_NAME = ""
-EMAIL = ""
-NETID = ""
-NETID_PASSWORD = ""
-DEFAULT_TIMEOUT = 100
-INTERVAL = 5
-
 class Bot():
-  def __init__(self, poll_name=None):
+
+
+  def __init__(self, poll_name, email, netid, password, default_timeout, interval):
     self.driver =  webdriver.Firefox()
     self.poll_name = poll_name
+    self.email = email
+    self.netid = netid 
+    self.password = password
+    self.default_timeout = default_timeout
+    self.interval = interval
   
+
   def login(self):
     print("logging in...")
     self.driver.get(f'https://pollev.com/home')
@@ -30,7 +31,7 @@ class Bot():
 
     print("entering email to email input")
     google_email_input = self.driver.find_element(By.XPATH, '//*[@id="identifierId"]')
-    google_email_input.send_keys(f"{EMAIL}")
+    google_email_input.send_keys(f"{self.email}")
 
     print("sending click to next")
     next_btn = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span")
@@ -38,11 +39,11 @@ class Bot():
 
     print("entering netid to netid input")
     netid_input = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="username"]')))
-    netid_input.send_keys(f"{NETID}")
+    netid_input.send_keys(f"{self.netid}")
 
     print("entering password to password input")
     password_input = self.driver.find_element(By.XPATH, '//*[@id="password"]')
-    password_input.send_keys(f"{NETID_PASSWORD}")
+    password_input.send_keys(f"{self.password}")
 
     print("sending click to login")
     login = self.driver.find_element(By.XPATH, "/html/body/div[2]/main/article/div/div[1]/form/fieldset/div/div[1]/input")
@@ -51,44 +52,58 @@ class Bot():
     print("waiting for 2FA")
 
     print("sending click to trust browser")
-    trust = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="trust-browser-button"]')))
+    trust = WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located((By.XPATH, '//*[@id="trust-browser-button"]')))
     trust.click()
 
     print("sending click to login with cornell id")
-    login_w_cornell = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/main/div/div[1]/p/a[1]')))
+    login_w_cornell = WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/main/div/div[1]/p/a[1]')))
     login_w_cornell.click()
     
     print("entering poll name")
-    poll_name = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '/html/body/pe-application/div/div/div/layout-plain/section/div/main/pollev-home-join/form/pe-text-field/div/label/input')))
-    poll_name.send_keys(f"{POLL_NAME}")
+    poll_name = WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located((By.XPATH, '/html/body/pe-application/div/div/div/layout-plain/section/div/main/pollev-home-join/form/pe-text-field/div/label/input')))
+    poll_name.send_keys(f"{self.poll_name}")
 
     print("sending click to join")
     join = self.driver.find_element(By.XPATH, '/html/body/pe-application/div/div/div/layout-plain/section/div/main/pollev-home-join/form/button')
     join.click()
 
+
   def poll_mc(self):
     while True:
       try:
         print("waiting for mc...")
-        mc = WebDriverWait(self.driver, DEFAULT_TIMEOUT).until(EC.presence_of_element_located((By.CLASS_NAME, 'component-response-multiple-choice__option__value')))
+        mc = WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located((By.CLASS_NAME, 'component-response-multiple-choice__option__value')))
         mc.click()
         print("sending click to mc")
 
         print("pausing search...")
-        time.sleep(INTERVAL)
+        time.sleep(self.interval)
 
       except KeyboardInterrupt:
         print("raising exception to quit")
       
-        
-
-
+      
   def end_session(self):
     print("closing...")
     self.driver.close()
     print("successfully closed")
     
+class Credentials():
+  def __init__(self):
+    if not self.is_credential_valid():
+      print("creating credential file")
+      self.create_credential
+    
+    print("credential file already exists")
+    
+  
+  def is_credential_valid(self):
+    pass
+  
+  def create_credential(self):
+    pass
 
+  
 
 if __name__ == "__main__":
   bot= Bot(poll_name="4820sp")
